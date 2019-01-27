8e854e871eb6dd22aec77a09b83011c4db39f96e
Lorie Ransom <ransom_design@yahoo.com>
2019-01-26 16:07:12 -0800

    commit progress before restructuring app into modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp'),
   sass = require('gulp-sass'),
-  livereload = require('gulp-livereload')
+  livereload = require('gulp-livereload'),
+  babel = require('gulp-babel');
+
 
   gulp.task('html', function() {
     return gulp.src('*.html')
@@ -14,17 +16,21 @@ gulp.task('sass', function () {
     .pipe(livereload());
 });
 
-  gulp.task('js', function() {
-    return gulp.dest('js')
-      .pipe(livereload());
-  });
+gulp.task('js', function() {
+  return gulp.src('js/src/scripts.js')
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe( gulp.dest('js') )
+    .pipe(livereload())
+});
 
-  gulp.task('watch', function() {
-    livereload.listen();
-    gulp.watch('css/styles.scss', gulp.series('sass'));
-    gulp.watch('**/*.js', gulp.series('js'));
-    gulp.watch('**/*.html', gulp.series('html'));
-  });
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('css/styles.scss', gulp.series('sass'));
+  gulp.watch('js/src/*.js', gulp.series('js'));
+  gulp.watch('**/*.html', gulp.series('html'));
+});
 
 gulp.task('webserver', function() {
   gulp.src(dest)
@@ -36,4 +42,4 @@ gulp.task('webserver', function() {
 });
 
 // removed webserver after gulp 4 upgrade - somehow messing with watch/sass compiling?
-gulp.task('default', gulp.series('html', 'sass', 'watch'));
+gulp.task('default', gulp.series('html', 'sass', 'js', 'watch'));
--- js/scripts.js
+++ js/scripts.js
@@ -1,99 +1,200 @@
-;(function(){
+"use strict";
 
-'use strict';
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-window.onload = () => {
-    
-    class Card {
-        constructor(word_type, spanish_singular, spanish_plural, english) {
-            this.spanish_singular = spanish_singular;
-            this.spanish_plural = spanish_plural;
-            this.english = english;
-            this.word_type = word_type;
-        }
-        getWordType(){
-            return this.word_type;
-        }
-        getEnglish() {
-            return this.english;
-        }
-        getSpanishSingular() {
-            return this.spanish_singular;
-        }
-        getSpanishPlural() {
-            return this.spanish_plural;
-        }
-    }
-    class Lesson {
-        constructor(){
-            this.cards
-            this.cardChoice = 0;
-        }
-        init(cardsData){ 
-            if (!cardsData) { console.log('no cards!'); return }
-            let tempCards = []
-            tempCards = cardsData.map( obj => {
-                return new Card(obj.word_type, obj.spanish_singular, obj.spanish_plural, obj.english)
-            }) 
-            this.cards = tempCards.sort(function() { return 0.5 - Math.random() }); //randomize the array
-            console.log(this.cards);
-            this.printCard();
-            this.setNewCardButton();
-        }
-        getCard() {
-            let c = this.cards[this.cardChoice];
-            this.cardChoice++;
-            return c;
-        }
-        printCard(){
-            let card = this.getCard();
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-            let reveal = document.getElementById("reveal");
-            let phrase = document.getElementById("phrase");
-            let plural = document.getElementById("revealplural");
-            let answer = document.getElementById("answer");
-            let wordtype = document.getElementById("wordtype");
-            let isSingular = true;
-            let revEnglish;
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-            //reset text areas 
-            phrase.innerText = '';
-            answer.innerText = '';
-            
-            phrase.innerText = card.getSpanishSingular();
-            wordtype.innerText = card.getWordType();
-            reveal.classList.remove("disabled");
+;
 
-            reveal.addEventListener('click', revEnglish =(e) => {
-                answer.innerText = card.getEnglish();
-                e.target.classList.add("disabled");
-                reveal.removeEventListener('click', revEnglish );
-            })
-            plural.addEventListener('click', () => {
-                let s = isSingular ? card.getSpanishPlural() : card.getSpanishSingular();
-                phrase.innerText = s;
-                isSingular = !isSingular;
-            })
+(function () {
+  'use strict';
+
+  window.onload = function () {
+    var Card =
+    /*#__PURE__*/
+    function () {
+      function Card(word_type, spanish_singular, spanish_plural, english) {
+        _classCallCheck(this, Card);
+
+        this.spanish_singular = spanish_singular;
+        this.spanish_plural = spanish_plural;
+        this.english = english;
+        this.word_type = word_type;
+      }
+
+      _createClass(Card, [{
+        key: "getPhrase",
+        value: function getPhrase() {
+          var lang = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'en';
+          return lang === 'en' ? this.english : this.spanish_singular;
         }
-        setNewCardButton(){
-            let newCard = document.getElementById("newcard");
-
-            newCard.addEventListener('click', (e)=>{
-                this.printCard();
-            })
+      }, {
+        key: "getWordType",
+        value: function getWordType() {
+          return this.word_type;
         }
-    }
+      }, {
+        key: "getSpanishPlural",
+        value: function getSpanishPlural() {
+          return this.spanish_plural;
+        }
+      }]);
 
-    fetch('data/json/vocab.json')
-        .then( (response) => {
-            return response.json()
-        })
-        .then( (myVocab) =>{
-            // console.log(JSON.stringify(myVocab));
-            let newLesson = new Lesson();
-            newLesson.init(myVocab);
-        })
-        .catch(error => console.error(error));
-    
-} //end window.onload;
+      return Card;
+    }();
+
+    var Lesson =
+    /*#__PURE__*/
+    function () {
+      function Lesson() {
+        _classCallCheck(this, Lesson);
+
+        this.cards;
+        this.cardChoice = 0;
+        this.lessonLanguage = 'sp';
+        this.nextBtn = null; //keep a reference to the next button event listener
+
+        this.isRevealed = false;
+        this.answerText;
+        this.answer;
+      }
+
+      _createClass(Lesson, [{
+        key: "init",
+        value: function init(cardsData) {
+          this.createCardList(cardsData);
+          this.setLessonLanguage();
+          this.printCard();
+          this.setNewCardButton();
+        }
+      }, {
+        key: "createCardList",
+        value: function createCardList(cardsData) {
+          if (!cardsData) {
+            return;
+          }
+
+          var tempCards = [];
+          tempCards = cardsData.map(function (obj) {
+            return new Card(obj.word_type, obj.spanish_singular, obj.spanish_plural, obj.english);
+          });
+          this.cards = tempCards.sort(function () {
+            return 0.5 - Math.random();
+          }); //randomize the array
+          // this.cards = this.cards.slice(0, 5); //temporarily reduce list for testing
+        }
+      }, {
+        key: "shuffleCardsList",
+        value: function shuffleCardsList() {}
+      }, {
+        key: "getCard",
+        value: function getCard() {
+          var c = this.cards[this.cardChoice];
+          this.cardChoice++;
+          return c;
+        }
+      }, {
+        key: "printCard",
+        value: function printCard() {
+          this.answerText = document.querySelector("#answer");
+          var card = this.getCard();
+          var testText = document.querySelector("#phrase");
+          var plural = document.querySelector("#revealplural");
+          var wordtype = document.querySelector("#wordtype");
+          var testTitleText = document.querySelector("#test-lang-title");
+          var ansTitleText = document.querySelector("#answer-lang-title");
+          var isSingular = true;
+          var next = document.querySelector("#next"); //reset text areas 
+
+          this.answerText.innerText = '';
+          this.answer = '';
+          testText.innerText = '';
+          next.innerText = 'Reveal Answer';
+          var phrase = '',
+              testTitle = '',
+              ansTitle = '';
+
+          if (this.lessonLanguage === 'en') {
+            testTitle = 'English';
+            ansTitle = 'Spanish';
+            phrase = card.getPhrase('en');
+            this.answer = card.getPhrase('sp');
+            plural.classList.add('hidden');
+          } else if (this.lessonLanguage === 'sp') {
+            testTitle = 'Spanish';
+            ansTitle = 'English';
+            phrase = card.getPhrase('sp');
+            this.answer = card.getPhrase('en');
+            plural.classList.remove('hidden');
+          }
+
+          wordtype.innerText = card.getWordType();
+          testTitleText.innerText = testTitle;
+          ansTitleText.innerText = ansTitle;
+          testText.innerText = phrase;
+          plural.addEventListener('click', function () {
+            var s = isSingular ? card.getSpanishPlural() : card.getPhrase('sp');
+            testText.innerText = s;
+            isSingular = !isSingular;
+          });
+        }
+      }, {
+        key: "setNewCardButton",
+        value: function setNewCardButton() {
+          var _this = this;
+
+          next.addEventListener('click', this.nextBtn = function (e) {
+            if (_this.cards.length === _this.cardChoice) {
+              next.innerText = 'Done!';
+              next.classList.add('disabled');
+            } //if revealed=true, go next, if not, then reveal, set this.reveal = true
+
+
+            if (_this.revealed === true) {
+              _this.revealed = false;
+
+              _this.printCard();
+
+              next.innerText = 'Reveal Answer';
+            } else {
+              _this.revealed = true;
+              _this.answerText.innerText = _this.answer;
+              next.innerText = 'Next Clue';
+            }
+          });
+        }
+      }, {
+        key: "setLessonLanguage",
+        value: function setLessonLanguage() {
+          var _this2 = this;
+
+          var radios = document.querySelectorAll('input[name="langopt"]');
+
+          for (var i = 0; i < radios.length; i++) {
+            radios[i].addEventListener('click', function (e) {
+              _this2.lessonLanguage = e.target.value;
+              _this2.cardChoice = 0; //reset to beginning
+
+              _this2.printCard();
+            });
+          }
+        }
+      }]);
+
+      return Lesson;
+    }();
+
+    fetch('data/json/vocab.json').then(function (response) {
+      return response.json();
+    }).then(function (myVocab) {
+      // console.log(JSON.stringify(myVocab));
+      var newLesson = new Lesson();
+      newLesson.init(myVocab);
+    }).catch(function (error) {
+      return console.error(error);
+    });
+  }; //end window.onload;
+
 })();
